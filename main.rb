b47d11eb80393fd5ec99d93e5b0665e4ef97678b
Albert Zhan <albert.cfzhan@gmail.com>
2022-07-20 13:31:10 -0700

    computer guesses + organized classes and modules

--- main.rb
+++ main.rb
@@ -1,63 +1,108 @@
 # frozen_string_literal: true
 
+# put unused by both functions back into the computer class
+# Is able to deal with the results of the guess
+module GiveResultsable
+  private
+
+  def win_message(code, guesser)
+    puts guesser == 'player_guesser' ? 'Congrats! You won!!!' : 'The computer guesssed your code!'
+    puts "The correct code was #{code}"
+  end
+
+  def loss_message(code, guesser)
+    puts guesser == 'player_guesser' ? 'O0f, you lost :(' : 'Nice, the computer was not able to guess your code.'
+    puts "The correct code was #{code}"
+  end
+
+  def print_results(correct_position, wrong_position)
+    puts "There was #{correct_position} in the right position with the corrrect number and #{wrong_position} with the right number but wrong position!"
+    puts '---------------------------------'
+    puts
+  end
+
+  def results(guess, guesses, code)
+    puts "Guess ##{guesses}"
+    correct_position = 0
+    for i in (0..3)
+      if guess.to_s[i] == code.to_s[i]
+        correct_position += 1
+      end
+    end
+    wrong_position = (guess.to_s.split('') & code.to_s.split('')).length - correct_position
+    print_results(correct_position, wrong_position)
+  end
+
+  def check_game_over(guess, guesses, code, guesser)
+    if guess == code
+      win_message(code, guesser)
+      self.game_over = true
+    elsif guess.zero?
+      loss_message(code, guesser)
+      self.game_over = true
+    else
+      results(guess, guesses, code)
+    end
+  end
+end
+
+# The computer which plays with the player
 class Computer
+  include GiveResultsable
+
   attr_reader :game_over, :guesses
 
   def initialize
     @guesses = 1
     @game_over = false
-    create_code
+    @code = create_code
+  end
+
+  def make_guess(code)
+    computer_guess = create_code.to_i
+    puts "The computer guessed #{computer_guess}"
+    check_game_over(computer_guess, @guesses, code, 'computer_guesser')
+    @guesses += 1
+    if @guesses > 12
+      puts 'The computer could not guess the code.'
+      @game_over = true
+    end
+    'guess made'
   end
 
   def recieve_guess(guess)
-    print_feedback(guess)
+    check_game_over(guess, @guesses, @code, 'player_guesser')
     @guesses += 1
-    if @guesses > 12
-      loss
+    if @guesses > 12 && !game_over
+      loss_message(@code)
+      @game_over = true
     end
+    'recieved guess'
   end
 
   private
 
-  def win
-    puts 'Congrats! You won!!!'
-    puts "The correct code was #{@code}"
-    @game_over = true
-  end
+  attr_writer :game_over
 
-  def loss
-    puts 'O0f, you lost :('
-    puts "The correct code was #{@code}"
-    @game_over = true
-  end
-
-  def print_feedback(guess) # print guess number
-    puts "Guess ##{@guesses}"
-    if guess == @code
-      win
-    elsif guess.zero?
-      loss
-    else
-      correct_position = 0
-      for i in (0..3)
-        if guess.to_s[i] == @code.to_s[i]
-          correct_position += 1
-        end
-      end
-      wrong_position = (guess.to_s.split('') & @code.to_s.split('')).length - correct_position
-      puts "You got #{correct_position} in the right position with the corrrect number and #{wrong_position} with the right number but wrong position!"
-      puts
-    end
-  end
-
-  def create_code # oh yeah this doesn't actually work what was i thinking lmao
+  def create_code
     digits = Array.new(4)
     4.times { digits.push(rand(1..6)) }
-    @code = digits.join('')
+    digits.join('')
   end
 end
 
-class Human
+# The human player which can input and interact with the program
+class Player
+  attr_reader :code
+
+  def enter_code # check input
+    puts "Enter a code for the computer to guess."
+    @code = gets.chomp.to_i
+  end
+
+  def check_valid_guess # put in module
+  end
+
   def guess # TODO: has to be int from 1111 to 6666
     gets.chomp.to_i
   end
@@ -65,11 +110,24 @@ end
 
 # TODO: module or instance function for printing instructions, or make some functions as part of the Game class/module
 
-puts "Guess the code in 12 tries, type 0 or enter to give up."
+# new game?
 
 computer = Computer.new
-player = Human.new
+player = Player.new
 
-until computer.game_over
-  computer.recieve_guess(player.guess)
+puts 'Enter 1 for code creator and 2 for guesser: '
+game_type = gets.chomp.to_i # check until 1 or 2
+if game_type == 1
+  player.enter_code
+  puts
+  until computer.game_over
+    computer.make_guess(player.code)
+    # wait for a bit
+  end
+else
+  puts 'Guess the code in 12 tries, type 0 or enter to give up.'
+  puts
+  until computer.game_over
+    computer.recieve_guess(player.guess)
+  end
 end
